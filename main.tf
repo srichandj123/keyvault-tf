1db17ec3883c106a69f33db9a7d483786975680b
srichandj123 <srichandbec@gmail.com>
2024-07-02 00:08:01 +0530

    FIxeeddd

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 module "rg-keyvault" {
   source  = "app.terraform.io/jakka/rg/module"
   version = "3.1.1"
-  region  = "Central US"
-  rgname  = "keyvault-rg"
+  loc     = "Central US"
+  rg_name = "keyvault-rg"
   tags = {
     Client = "OCC"
   }
@@ -11,9 +11,9 @@ module "rg-keyvault" {
 data "azurerm_client_config" "current" {}
 resource "azurerm_key_vault" "kv" {
   name                = "jakk-key-vault"
-  location            = module.rg-keyvault.region
-  resource_group_name = module.rg-keyvault.rgname
-  sku_name            = "Standard"
+  location            = module.rg-keyvault.location
+  resource_group_name = module.rg-keyvault.rg_name
+  sku_name            = "standard"
   tenant_id           = data.azurerm_client_config.current.tenant_id
   access_policy {
     tenant_id = data.azurerm_client_config.current.tenant_id

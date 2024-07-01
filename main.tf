13b53c50fe1549e7bedd96c1a407ee8e7e4f2bfe
srichandj123 <srichandbec@gmail.com>
2024-07-01 23:55:41 +0530

    Modified the Permissions

--- main.tf
+++ main.tf
@@ -13,14 +13,22 @@ resource "azurerm_key_vault" "kv" {
   name                = "jakk-key-vault"
   location            = module.rg-keyvault.region
   resource_group_name = module.rg-keyvault.rgname
-  sku_name            = Standard
+  sku_name            = "Standard"
   tenant_id           = data.azurerm_client_config.current.tenant_id
   access_policy {
-    tenant_id           = data.azurerm_client_config.current.tenant_id
-    object_id           = data.azurerm_client_config.current.client_id
-    key_permissions     = [Get, ]
-    secret_permissions  = [Get, ]
-    storage_permissions = [Get, ]
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.client_id
+    key_permissions = [
+      "Get", "List", "Delete", "Create"
+    ]
+
+    secret_permissions = [
+      "Get",
+    ]
+
+    storage_permissions = [
+      "Get",
+    ]
   }
 
 }
